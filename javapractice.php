90ce03f8112c58992d96827f5ac658c54e65f4e1
vimashcoder678 <vimashprajapati03@gmail.com>
2023-02-18 12:40:07 +0530

    My portfolio

---
+++ javapractice.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>javascript practice 
+	</title>
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+<script type="text/javascript" src="js/practice.js">
+</script>
+</body>
+</html>
---
+++ unnessory/dign.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+</head>
+<body>
+	<table class="table">
+  <thead>
+    <tr >
+      <th scope="col" class="p-4">#</th>
+      <th scope="col" class="p-4">First</th>
+      <th scope="col" class="p-4">Last</th>
+      <th scope="col" class="p-4">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+</body>
+</html>
